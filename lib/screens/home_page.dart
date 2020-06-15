a16d55b4a5adef6b1a978016cd4cb7d93ef44adf
Souvik Biswas <sbis1999@gmail.com>
2020-06-15 18:18:00 +0530

    Add featured title

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -14,127 +14,6 @@ class _HomePageState extends State<HomePage> {
     var screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      // appBar: PreferredSize(
-      //   preferredSize: Size(screenSize.width, 1000),
-      //   child: Container(
-      //     color: Colors.blue,
-      //     child: Padding(
-      //       padding: EdgeInsets.all(20),
-      //       child: Row(
-      //         children: [
-      //           Text('EXPLORE'),
-      //           Expanded(
-      //             child: Row(
-      //               mainAxisAlignment: MainAxisAlignment.center,
-      //               children: [
-      //                 InkWell(
-      //                   onHover: (value) {
-      //                     setState(() {
-      //                       value
-      //                           ? _isHovering[0] = true
-      //                           : _isHovering[0] = false;
-      //                     });
-      //                   },
-      //                   onTap: () {},
-      //                   child: Column(
-      //                     mainAxisSize: MainAxisSize.min,
-      //                     children: [
-      //                       Text(
-      //                         'Discover',
-      //                         style: TextStyle(
-      //                           color: _isHovering[0]
-      //                               ? Colors.blue[100]
-      //                               : Colors.white,
-      //                         ),
-      //                       ),
-      //                       SizedBox(height: 5),
-      //                       Visibility(
-      //                         maintainAnimation: true,
-      //                         maintainState: true,
-      //                         maintainSize: true,
-      //                         visible: _isHovering[0],
-      //                         child: Container(
-      //                           height: 2,
-      //                           width: 20,
-      //                           color: Colors.white,
-      //                         ),
-      //                       )
-      //                     ],
-      //                   ),
-      //                 ),
-      //                 SizedBox(width: screenSize.width / 20),
-      //                 InkWell(
-      //                   onHover: (value) {
-      //                     setState(() {
-      //                       value
-      //                           ? _isHovering[1] = true
-      //                           : _isHovering[1] = false;
-      //                     });
-      //                   },
-      //                   onTap: () {},
-      //                   child: Column(
-      //                     mainAxisSize: MainAxisSize.min,
-      //                     children: [
-      //                       Text(
-      //                         'Contact Us',
-      //                         style: TextStyle(
-      //                           color: _isHovering[1]
-      //                               ? Colors.blue[100]
-      //                               : Colors.white,
-      //                         ),
-      //                       ),
-      //                       SizedBox(height: 5),
-      //                       Visibility(
-      //                         maintainAnimation: true,
-      //                         maintainState: true,
-      //                         maintainSize: true,
-      //                         visible: _isHovering[1],
-      //                         child: Container(
-      //                           height: 2,
-      //                           width: 20,
-      //                           color: Colors.white,
-      //                         ),
-      //                       )
-      //                     ],
-      //                   ),
-      //                 ),
-      //               ],
-      //             ),
-      //           ),
-      //           InkWell(
-      //             onHover: (value) {
-      //               setState(() {
-      //                 value ? _isHovering[2] = true : _isHovering[2] = false;
-      //               });
-      //             },
-      //             onTap: () {},
-      //             child: Text(
-      //               'Sign Up',
-      //               style: TextStyle(
-      //                   color: _isHovering[2] ? Colors.white : Colors.black),
-      //             ),
-      //           ),
-      //           SizedBox(
-      //             width: screenSize.width / 50,
-      //           ),
-      //           InkWell(
-      //             onHover: (value) {
-      //               setState(() {
-      //                 value ? _isHovering[3] = true : _isHovering[3] = false;
-      //               });
-      //             },
-      //             onTap: () {},
-      //             child: Text(
-      //               'Login',
-      //               style: TextStyle(
-      //                   color: _isHovering[3] ? Colors.white : Colors.black),
-      //             ),
-      //           ),
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
       body: Stack(
         children: [
           Container(
@@ -281,6 +160,35 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: screenSize.height * 0.55,
+              left: screenSize.width /15,
+            ),
+            child: Container(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Featured',
+                        style: GoogleFonts.montserrat(fontSize: 40, fontWeight: FontWeight.w500),
+                      ),
+                      Expanded(
+                        child: Text(
+                          'Unique wildlife tours & destinations',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row()
+                ],
               ),
             ),
           ),
