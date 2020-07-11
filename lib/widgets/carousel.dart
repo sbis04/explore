b19c47df977433ad158801408ca292bb3255da55
Souvik Biswas <sbis1999@gmail.com>
2020-07-11 19:25:01 +0530

    Carousal animation

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -12,7 +12,8 @@ class _DestinationCarouselState extends State<DestinationCarousel> {
 
   final CarouselController _controller = CarouselController();
 
-  List _isHovering = [false, false, false, false, false, false, false, false];
+  List _isHovering = [false, false, false, false, false, false, false];
+  List _isSelected = [true, false, false, false, false, false, false];
 
   int _current = 0;
 
@@ -64,6 +65,13 @@ class _DestinationCarouselState extends State<DestinationCarousel> {
               onPageChanged: (index, reason) {
                 setState(() {
                   _current = index;
+                  for (int i = 0; i < imageSliders.length; i++) {
+                    if (i == index) {
+                      _isSelected[i] = true;
+                    } else {
+                      _isSelected[i] = false;
+                    }
+                  }
                 });
               }),
           carouselController: _controller,
@@ -103,32 +111,57 @@ class _DestinationCarouselState extends State<DestinationCarousel> {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         for (int i = 0; i < places.length; i++)
-                          Container(
-                            child: InkWell(
-                              splashColor: Colors.transparent,
-                              hoverColor: Colors.transparent,
-                              onHover: (value) {
-                                setState(() {
-                                  value
-                                      ? _isHovering[i] = true
-                                      : _isHovering[i] = false;
-                                });
-                              },
-                              onTap: () {},
-                              child: Padding(
-                                padding: EdgeInsets.only(
-                                    top: screenSize.height / 80,
-                                    bottom: screenSize.height / 80),
-                                child: Text(
-                                  places[i],
-                                  style: TextStyle(
-                                    color: _isHovering[i]
-                                        ? Colors.blueGrey[900]
-                                        : Colors.blueGrey,
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              InkWell(
+                                splashColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                onHover: (value) {
+                                  setState(() {
+                                    value
+                                        ? _isHovering[i] = true
+                                        : _isHovering[i] = false;
+                                  });
+                                },
+                                onTap: () {
+                                  _controller.animateToPage(i);
+                                },
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      top: screenSize.height / 80,
+                                      bottom: screenSize.height / 90),
+                                  child: Text(
+                                    places[i],
+                                    style: TextStyle(
+                                      color: _isHovering[i]
+                                          ? Colors.blueGrey[900]
+                                          : Colors.blueGrey,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
+                              Visibility(
+                                maintainSize: true,
+                                maintainAnimation: true,
+                                maintainState: true,
+                                visible: _isSelected[i],
+                                child: AnimatedOpacity(
+                                  duration: Duration(milliseconds: 400),
+                                  opacity: _isSelected[i] ? 1 : 0,
+                                  child: Container(
+                                    height: 5,
+                                    decoration: BoxDecoration(
+                                      color: Colors.blueGrey,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      ),
+                                    ),
+                                    width: screenSize.width / 10,
+                                  ),
+                                ),
+                              )
+                            ],
                           ),
 
                         // InkWell(
