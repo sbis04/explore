eb98ad3e787c24766c79d7a53b3668653a975139
Souvik Biswas <sbis1999@gmail.com>
2020-07-12 09:17:11 +0530

    Refactor code

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:explore/widgets/bottom_bar_column.dart';
 import 'package:explore/widgets/carousel.dart';
 import 'package:explore/widgets/info_text.dart';
+import 'package:explore/widgets/top_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -40,136 +41,7 @@ class _HomePageState extends State<HomePage> {
       extendBodyBehindAppBar: true,
       appBar: PreferredSize(
         preferredSize: Size(screenSize.width, 1000),
-        child: Container(
-          color: Colors.blueGrey[900].withOpacity(_opacity),
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'EXPLORE',
-                  style: GoogleFonts.montserrat(
-                    color: Colors.blueGrey[100],
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    letterSpacing: 3,
-                  ),
-                ),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: screenSize.width / 8),
-                      InkWell(
-                        onHover: (value) {
-                          setState(() {
-                            value
-                                ? _isHovering[0] = true
-                                : _isHovering[0] = false;
-                          });
-                        },
-                        onTap: () {},
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              'Discover',
-                              style: TextStyle(
-                                color: _isHovering[0]
-                                    ? Colors.blue[200]
-                                    : Colors.white,
-                              ),
-                            ),
-                            SizedBox(height: 5),
-                            Visibility(
-                              maintainAnimation: true,
-                              maintainState: true,
-                              maintainSize: true,
-                              visible: _isHovering[0],
-                              child: Container(
-                                height: 2,
-                                width: 20,
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      SizedBox(width: screenSize.width / 20),
-                      InkWell(
-                        onHover: (value) {
-                          setState(() {
-                            value
-                                ? _isHovering[1] = true
-                                : _isHovering[1] = false;
-                          });
-                        },
-                        onTap: () {},
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              'Contact Us',
-                              style: TextStyle(
-                                color: _isHovering[1]
-                                    ? Colors.blue[200]
-                                    : Colors.white,
-                              ),
-                            ),
-                            SizedBox(height: 5),
-                            Visibility(
-                              maintainAnimation: true,
-                              maintainState: true,
-                              maintainSize: true,
-                              visible: _isHovering[1],
-                              child: Container(
-                                height: 2,
-                                width: 20,
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                InkWell(
-                  onHover: (value) {
-                    setState(() {
-                      value ? _isHovering[2] = true : _isHovering[2] = false;
-                    });
-                  },
-                  onTap: () {},
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(
-                      color: _isHovering[2] ? Colors.white : Colors.white70,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: screenSize.width / 50,
-                ),
-                InkWell(
-                  onHover: (value) {
-                    setState(() {
-                      value ? _isHovering[3] = true : _isHovering[3] = false;
-                    });
-                  },
-                  onTap: () {},
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: _isHovering[3] ? Colors.white : Colors.white70,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+        child: TopBar(_opacity),
       ),
       body: SingleChildScrollView(
         controller: _scrollController,
---
+++ lib/widgets/top_bar.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TopBar extends StatefulWidget {
+  final double opacity;
+
+  TopBar(this.opacity);
+
+  @override
+  _TopBarState createState() => _TopBarState();
+}
+
+class _TopBarState extends State<TopBar> {
+  final List _isHovering = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
+    return PreferredSize(
+      preferredSize: Size(screenSize.width, 1000),
+      child: Container(
+        color: Colors.blueGrey[900].withOpacity(widget.opacity),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'EXPLORE',
+                style: GoogleFonts.montserrat(
+                  color: Colors.blueGrey[100],
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 3,
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(width: screenSize.width / 8),
+                    InkWell(
+                      onHover: (value) {
+                        setState(() {
+                          value
+                              ? _isHovering[0] = true
+                              : _isHovering[0] = false;
+                        });
+                      },
+                      onTap: () {},
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Discover',
+                            style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Colors.blue[200]
+                                  : Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Visibility(
+                            maintainAnimation: true,
+                            maintainState: true,
+                            maintainSize: true,
+                            visible: _isHovering[0],
+                            child: Container(
+                              height: 2,
+                              width: 20,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: screenSize.width / 20),
+                    InkWell(
+                      onHover: (value) {
+                        setState(() {
+                          value
+                              ? _isHovering[1] = true
+                              : _isHovering[1] = false;
+                        });
+                      },
+                      onTap: () {},
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Contact Us',
+                            style: TextStyle(
+                              color: _isHovering[1]
+                                  ? Colors.blue[200]
+                                  : Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Visibility(
+                            maintainAnimation: true,
+                            maintainState: true,
+                            maintainSize: true,
+                            visible: _isHovering[1],
+                            child: Container(
+                              height: 2,
+                              width: 20,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              InkWell(
+                onHover: (value) {
+                  setState(() {
+                    value ? _isHovering[2] = true : _isHovering[2] = false;
+                  });
+                },
+                onTap: () {},
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: _isHovering[2] ? Colors.white : Colors.white70,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: screenSize.width / 50,
+              ),
+              InkWell(
+                onHover: (value) {
+                  setState(() {
+                    value ? _isHovering[3] = true : _isHovering[3] = false;
+                  });
+                },
+                onTap: () {},
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: _isHovering[3] ? Colors.white : Colors.white70,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
