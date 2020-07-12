8fe91f37ed33fae4718dbcb6d2f6aa2f49e0ee4c
Souvik Biswas <sbis1999@gmail.com>
2020-07-12 15:41:00 +0530

    Make the feature heading responsive

--- lib/widgets/featured_heading.dart
+++ lib/widgets/featured_heading.dart
@@ -1,3 +1,4 @@
+import 'package:explore/widgets/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,24 +9,45 @@ class FeaturedHeading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text(
-          'Featured',
-          style: GoogleFonts.montserrat(
-            fontSize: 40,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        Expanded(
-          child: Text(
-            'Unique wildlife tours & destinations',
-            textAlign: TextAlign.end,
-          ),
-        ),
-      ],
-    );
+    return ResponsiveWidget.isSmallScreen(context)
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(),
+              Text(
+                'Featured',
+                style: GoogleFonts.montserrat(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Unique wildlife tours & destinations',
+                textAlign: TextAlign.end,
+              ),
+              SizedBox(height: 10),
+            ],
+          )
+        : Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Featured',
+                style: GoogleFonts.montserrat(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'Unique wildlife tours & destinations',
+                  textAlign: TextAlign.end,
+                ),
+              ),
+            ],
+          );
   }
 }
