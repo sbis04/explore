bfdf331e72ac08fca62d4a175e7fbf38a25ff1fd
Souvik Biswas <sbis1999@gmail.com>
2020-07-22 13:31:48 +0530

    Add dynamic theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/home_page.dart';
@@ -9,14 +10,51 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Explore',
-      theme: ThemeData(
-        // primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return DynamicTheme(
+      defaultBrightness: Brightness.light,
+      data: (brightness) {
+        return brightness == Brightness.light
+            ? ThemeData(
+                primarySwatch: Colors.blueGrey,
+                backgroundColor: Colors.white,
+                cardColor: Colors.blueGrey[50],
+                primaryTextTheme: TextTheme(
+                  button: TextStyle(
+                    color: Colors.blueGrey,
+                    decorationColor: Colors.blueGrey[300],
+                  ),
+                  subtitle1: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                bottomAppBarColor: Colors.blueGrey[900],
+                iconTheme: IconThemeData(color: Colors.blueGrey),
+                brightness: brightness,
+              )
+            : ThemeData(
+                primarySwatch: Colors.blueGrey,
+                backgroundColor: Colors.blueGrey[900],
+                cardColor: Colors.black,
+                primaryTextTheme: TextTheme(
+                  button: TextStyle(
+                    color: Colors.blueGrey[200],
+                    decorationColor: Colors.blueGrey[50],
+                  ),
+                  subtitle1: TextStyle(
+                    color: Colors.blueGrey[300],
+                  ),
+                ),
+                bottomAppBarColor: Colors.black,
+                iconTheme: IconThemeData(color: Colors.blueGrey[200]),
+                brightness: brightness,
+              );
+      },
+      themedWidgetBuilder: (context, data) => MaterialApp(
+        title: 'Explore',
+        theme: data,
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
     );
   }
 }
