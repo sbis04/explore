26dfefcc51444c8bb59125c1911ab11e66ec54b4
Souvik Biswas <sbis1999@gmail.com>
2020-07-30 09:25:21 +0530

    Show indicator while processing

--- lib/widgets/google_sign_in_button.dart
+++ lib/widgets/google_sign_in_button.dart
@@ -1,7 +1,15 @@
+import 'package:explore/screens/home_page.dart';
 import 'package:explore/utils/sign_in.dart';
 import 'package:flutter/material.dart';
 
-class GoogleButton extends StatelessWidget {
+class GoogleButton extends StatefulWidget {
+  @override
+  _GoogleButtonState createState() => _GoogleButtonState();
+}
+
+class _GoogleButtonState extends State<GoogleButton> {
+  bool _isProcessing = false;
+
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
@@ -15,17 +23,26 @@ class GoogleButton extends StatelessWidget {
       child: OutlineButton(
         highlightColor: Colors.blueGrey[100],
         splashColor: Colors.blueGrey[200],
-        onPressed: () {
-          signInWithGoogle().whenComplete(() {
-            // Navigator.of(context).push(
-            //   MaterialPageRoute(
-            //     builder: (context) {
-            //       return NamePage();
-            //     },
-            //   ),
-            // ).catchError(
-            //   (e) => print('SIGN IN ERROR: $e'),
-            // );
+        onPressed: () async {
+          setState(() {
+            _isProcessing = true;
+          });
+          await signInWithGoogle().then((result) {
+            print(result);
+            Navigator.of(context).pop();
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                fullscreenDialog: true,
+                builder: (context) => HomePage(
+                  loggedIn: true,
+                ),
+              ),
+            );
+          }).catchError((error) {
+            print('Registration Error: $error');
+          });
+          setState(() {
+            _isProcessing = false;
           });
         },
         shape: RoundedRectangleBorder(
@@ -36,26 +53,32 @@ class GoogleButton extends StatelessWidget {
         // borderSide: BorderSide(color: Colors.blueGrey, width: 3),
         child: Padding(
           padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image(
-                image: AssetImage("assets/images/google_logo.png"),
-                height: 30.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Text(
-                  'Continue with Google',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.blueGrey,
+          child: _isProcessing
+              ? CircularProgressIndicator(
+                  valueColor: new AlwaysStoppedAnimation<Color>(
+                    Colors.blueGrey,
                   ),
+                )
+              : Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage("assets/images/google_logo.png"),
+                      height: 30.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        'Continue with Google',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
         ),
       ),
     );
