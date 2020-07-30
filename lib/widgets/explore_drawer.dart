ddc2dd08db4d85a452d8a5801b602ff7a31d5c47
Souvik Biswas <sbis1999@gmail.com>
2020-07-30 10:11:08 +0530

    Added sign out button

--- lib/widgets/explore_drawer.dart
+++ lib/widgets/explore_drawer.dart
@@ -1,10 +1,20 @@
+import 'package:explore/screens/home_page.dart';
+import 'package:explore/utils/sign_in.dart';
 import 'package:flutter/material.dart';
 
-class ExploreDrawer extends StatelessWidget {
+import 'auth_dialog.dart';
+
+class ExploreDrawer extends StatefulWidget {
   const ExploreDrawer({
     Key key,
   }) : super(key: key);
 
+  @override
+  _ExploreDrawerState createState() => _ExploreDrawerState();
+}
+
+class _ExploreDrawerState extends State<ExploreDrawer> {
+  bool _isProcessing = false;
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -16,32 +26,113 @@ class ExploreDrawer extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Container(
-                width: double.maxFinite,
-                child: FlatButton(
-                  color: Colors.black,
-                  hoverColor: Colors.blueGrey[800],
-                  highlightColor: Colors.blueGrey[700],
-                  onPressed: () {},
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      top: 15.0,
-                      bottom: 15.0,
-                    ),
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
+              userEmail == null
+                  ? Container(
+                      width: double.maxFinite,
+                      child: FlatButton(
+                        color: Colors.black,
+                        hoverColor: Colors.blueGrey[800],
+                        highlightColor: Colors.blueGrey[700],
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => AuthDialog(),
+                          );
+                        },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            top: 15.0,
+                            bottom: 15.0,
+                          ),
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
                       ),
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundImage:
+                              imageUrl != null ? NetworkImage(imageUrl) : null,
+                          child: imageUrl == null
+                              ? Icon(
+                                  Icons.account_circle,
+                                  size: 40,
+                                )
+                              : Container(),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          name ?? userEmail,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white70,
+                          ),
+                        )
+                      ],
                     ),
-                  ),
-                ),
-              ),
               SizedBox(height: 20),
+              userEmail != null
+                  ? Container(
+                      width: double.maxFinite,
+                      child: FlatButton(
+                        color: Colors.black,
+                        hoverColor: Colors.blueGrey[800],
+                        highlightColor: Colors.blueGrey[700],
+                        onPressed: _isProcessing
+                            ? null
+                            : () async {
+                                setState(() {
+                                  _isProcessing = true;
+                                });
+                                await signOut().then((result) {
+                                  print(result);
+                                  Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                      fullscreenDialog: true,
+                                      builder: (context) => HomePage(),
+                                    ),
+                                  );
+                                }).catchError((error) {
+                                  print('Registration Error: $error');
+                                });
+                                setState(() {
+                                  _isProcessing = false;
+                                });
+                              },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            top: 15.0,
+                            bottom: 15.0,
+                          ),
+                          child: _isProcessing
+                              ? CircularProgressIndicator()
+                              : Text(
+                                  'Sign out',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                        ),
+                      ),
+                    )
+                  : Container(),
+              userEmail != null ? SizedBox(height: 20) : Container(),
               InkWell(
                 onTap: () {},
                 child: Text(
