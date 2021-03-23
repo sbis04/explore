8870fdd2913fa64994e7535ccb564c9fb4abb38b
Souvik Biswas <sbis1999@gmail.com>
2021-03-23 17:22:28 +0530

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Explore',
       theme: ThemeData(
-        // primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
