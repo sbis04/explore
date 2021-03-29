8aa6bb611fd8e8fb958cac1a4a7d3ac37790bf5e
Souvik Biswas <sbis1999@gmail.com>
2021-03-29 21:31:18 +0530

    Initialize the dynamic theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
-import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
 import 'package:explore/utils/authentication.dart';
+import 'package:explore/utils/theme_data.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    EasyDynamicThemeWidget(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -28,61 +33,13 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return DynamicTheme(
-      defaultBrightness: Brightness.light,
-      data: (brightness) {
-        return brightness == Brightness.light
-            ? ThemeData(
-                primarySwatch: Colors.blueGrey,
-                backgroundColor: Colors.white,
-                cardColor: Colors.blueGrey[50],
-                primaryTextTheme: TextTheme(
-                  button: TextStyle(
-                    color: Colors.blueGrey,
-                    decorationColor: Colors.blueGrey[300],
-                  ),
-                  subtitle2: TextStyle(
-                    color: Colors.blueGrey[900],
-                  ),
-                  subtitle1: TextStyle(
-                    color: Colors.black,
-                  ),
-                  headline1: TextStyle(color: Colors.blueGrey[800]),
-                ),
-                bottomAppBarColor: Colors.blueGrey[900],
-                iconTheme: IconThemeData(color: Colors.blueGrey),
-                brightness: brightness,
-              )
-            : ThemeData(
-                primarySwatch: Colors.blueGrey,
-                backgroundColor: Colors.blueGrey[900],
-                cardColor: Colors.black,
-                primaryTextTheme: TextTheme(
-                  button: TextStyle(
-                    color: Colors.blueGrey[200],
-                    decorationColor: Colors.blueGrey[50],
-                  ),
-                  subtitle2: TextStyle(
-                    color: Colors.white,
-                  ),
-                  subtitle1: TextStyle(
-                    color: Colors.blueGrey[300],
-                  ),
-                  headline1: TextStyle(
-                    color: Colors.white70,
-                  ),
-                ),
-                bottomAppBarColor: Colors.black,
-                iconTheme: IconThemeData(color: Colors.blueGrey[200]),
-                brightness: brightness,
-              );
-      },
-      themedWidgetBuilder: (context, data) => MaterialApp(
-        title: 'Explore',
-        theme: data,
-        debugShowCheckedModeBanner: false,
-        home: HomePage(),
-      ),
+    return MaterialApp(
+      title: 'Explore',
+      theme: lightThemeData,
+      darkTheme: darkThemeData,
+      debugShowCheckedModeBanner: false,
+      themeMode: EasyDynamicTheme.of(context).themeMode,
+      home: HomePage(),
     );
   }
 }
