153c0b5e6b33ea989a1783c7c3888fcecc0f4c40
Souvik Biswas <sbis1999@gmail.com>
2021-03-29 21:45:43 +0530

    Update auth to support web google sign in

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -3,14 +3,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
-String uid;
-String name;
-String userEmail;
-String imageUrl;
+String? uid;
+String? name;
+String? userEmail;
+String? imageUrl;
 
 /// For checking if the user is already signed into the
 /// app using Google Sign In
@@ -20,7 +21,7 @@ Future getUser() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool authSignedIn = prefs.getBool('auth') ?? false;
 
-  final User user = _auth.currentUser;
+  final User? user = _auth.currentUser;
 
   if (authSignedIn == true) {
     if (user != null) {
@@ -37,108 +38,125 @@ Future getUser() async {
 ///
 /// Retrieves some general user related information
 /// from their Google account for ease of the login process
-Future<String> signInWithGoogle() async {
+Future<User?> signInWithGoogle() async {
   await Firebase.initializeApp();
 
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+  User? user;
 
-  final AuthCredential credential = GoogleAuthProvider.credential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
+  if (kIsWeb) {
+    GoogleAuthProvider authProvider = GoogleAuthProvider();
 
-  final UserCredential userCredential =
-      await _auth.signInWithCredential(credential);
-  final User user = userCredential.user;
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithPopup(authProvider);
+
+      user = userCredential.user;
+    } catch (e) {
+      print(e);
+    }
+  } else {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        final UserCredential userCredential =
+            await _auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          print('The account already exists with a different credential.');
+        } else if (e.code == 'invalid-credential') {
+          print('Error occurred while accessing credentials. Try again.');
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
 
   if (user != null) {
-    // Checking if email and name is null
-    assert(user.uid != null);
-    assert(user.email != null);
-    assert(user.displayName != null);
-    assert(user.photoURL != null);
-
     uid = user.uid;
     name = user.displayName;
     userEmail = user.email;
     imageUrl = user.photoURL;
 
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final User currentUser = _auth.currentUser;
-    assert(user.uid == currentUser.uid);
-
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool('auth', true);
-
-    return 'Google sign in successful, User UID: ${user.uid}';
   }
 
-  return null;
+  return user;
 }
 
-Future<String> registerWithEmailPassword(String email, String password) async {
+Future<User?> registerWithEmailPassword(String email, String password) async {
   await Firebase.initializeApp();
+  User? user;
 
-  final UserCredential userCredential =
-      await _auth.createUserWithEmailAndPassword(
-    email: email,
-    password: password,
-  );
+  try {
+    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
 
-  final User user = userCredential.user;
+    user = userCredential.user;
 
-  if (user != null) {
-    // checking if uid or email is null
-    assert(user.uid != null);
-    assert(user.email != null);
+    if (user != null) {
+      uid = user.uid;
+      userEmail = user.email;
+    }
 
-    uid = user.uid;
-    userEmail = user.email;
-
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    return 'Successfully registered, User UID: ${user.uid}';
+    return null;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+  } catch (e) {
+    print(e);
   }
 
-  return null;
+  return user;
 }
 
-Future<String> signInWithEmailPassword(String email, String password) async {
+Future<User?> signInWithEmailPassword(String email, String password) async {
   await Firebase.initializeApp();
+  User? user;
 
-  final UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-    email: email,
-    password: password,
-  );
+  try {
+    UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    user = userCredential.user;
 
-  final User user = userCredential.user;
+    if (user != null) {
+      uid = user.uid;
+      userEmail = user.email;
 
-  if (user != null) {
-    // checking if uid or email is null
-    assert(user.uid != null);
-    assert(user.email != null);
-
-    uid = user.uid;
-    userEmail = user.email;
-
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final User currentUser = _auth.currentUser;
-    assert(user.uid == currentUser.uid);
-
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool('auth', true);
-
-    return 'Successfully logged in, User UID: ${user.uid}';
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setBool('auth', true);
+    }
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'user-not-found') {
+      print('No user found for that email.');
+    } else if (e.code == 'wrong-password') {
+      print('Wrong password provided.');
+    }
   }
 
-  return null;
+  return user;
 }
 
 Future<String> signOut() async {
